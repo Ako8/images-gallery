bbd7383a6904028269842d1ff9b194b075b87492
Ako <akogachechiladze22@gmail.com>
2024-01-13 20:14:51 +0400

    initial header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from 'react-bootstrap';
+
+function Header(props) {
+
+    const {title} = props;
+
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
+        </Navbar>
+    );
+}
+
+
+export default Header;
